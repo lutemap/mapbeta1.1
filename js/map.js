c052688c1b3d0089d6db6de0e0e16646d94b2fbe
lutemap <tunahan_alan@hotmail.com>
2025-06-07 02:26:45 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -43,11 +43,6 @@ var esriSatellite = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/s
   maxZoom: 28
 });
 
-var ohm = L.tileLayer('https://tiles.openhistoricalmap.org/ohm/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenHistoricalMap contributors',
-  maxZoom: 22
-});
-
 var baseMaps = {
   "ESRI Street": esriStreet,
   "ESRI Satellite": esriSatellite,
